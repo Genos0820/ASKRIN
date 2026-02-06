27c9296d3a7a6958676bd2b5a373e73e094b9064
ptawri <ptiwari@infoblox>
2026-02-07 01:35:33 +0530

    Update changes

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,7 @@ export default function Page() {
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.webp"
           />
           <h1 className="text-4xl md:text-6xl my-4 text-center">
-            Will you be my valentine Mrinal(Ladoo)?
+            Will you go out with me Mrinal(Ladoo)?
           </h1>
           <div className="flex flex-wrap justify-center gap-2 items-center">
             <button
